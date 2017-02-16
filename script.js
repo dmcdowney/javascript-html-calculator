cb5c0e1a7fcaab0d226fe2fec174582463ed4dbb
Dyamond <dmcdowney@wlapcs.org>
2017-02-16 17:58:55 +0000

    my first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function clearDisplay(){
+    document.getElementById("display-bar").innerHTML = "";
+}
+
+function display(userInput){
+    document.getElementById("display-bar").innerHTML = userInput;
+}
+
+function calculator(){
+   var answer = eval(document.getElementById("display-bar").innerHTML);
+   document.getElementById("display-bar").innerHTML += answer;
+}
